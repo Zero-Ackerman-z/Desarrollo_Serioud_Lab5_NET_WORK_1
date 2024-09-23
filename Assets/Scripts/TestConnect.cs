9f6ea9479e0a710568d97fdbc402e5f7ab49ab37
Zero-Ackerman-z <144136238+Zero-Ackerman-z@users.noreply.github.com>
2024-09-23 12:54:42 -0500

    Initial commit

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class TestConnect: MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to server.");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to server.");
+        print(PhotonNetwork.LocalPlayer.NickName);
+    }
+    public override void OnDisconnected(DisconnectCause Cause)
+    {
+        print("Disconnected from server for reason" + Cause.ToString()) ;
+    }
+}
